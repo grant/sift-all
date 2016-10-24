cbc132344c67b2092e8019ea6f3b402ffecea0f6
Grant Timmerman <granttimmerman@gmail.com>
2016-10-23 23:13:56 -0700

    Added dependency

--- java/src/main/java/test/Main.java
+++ java/src/main/java/test/Main.java
@@ -1,10 +1,13 @@
 package test;
 
+
+import com.mcac0006.siftscience.event.domain.AddItemToCart;
+
 /**
  * test
  */
 public class Main {
     public static void main(String ...args) {
-        System.out.println("hi");
+        new AddItemToCart();
     }
 }
